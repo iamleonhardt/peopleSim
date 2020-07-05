15fad91dde8665ea42564f7cdc64415c271c6a58
Bill Leonhardt <iamleonhardt@gmail.com>
2020-07-05 16:34:20 -0700

    setting up to extend People to npc and pc

--- peopleData.js
+++ peopleData.js
@@ -36,3 +36,21 @@ let peopleArr = [
   "April",
   "Amanda",
 ];
+
+//  Model of person
+//  {
+//   name: Jack,
+//   id: name + "_" + i,
+//   socialDistance = 150,
+//   emotionLevel: 0, // num between -5 and 5
+//   radius: getRanNum(5, 10), // num between 5 and 10
+//   x: getRanNum(0 + this.radius, canvas.width), // num between 0 and canvas.width
+//   y: getRanNum(0 + this.radius, canvas.height), // num between 0 and canvas.height
+//   speed: 3, // num between 1 and 5
+//   color: this.getEmotionColor(this.emotionLevel),
+//   speakArr: [], // {words: "string", removeTime: num}
+//   chatTimer: 0,
+//   awarenessMap: {}, // {personId : {durationOfAwareness: num of how long im aware of them, person: personObj}}
+//   longTermAwarenessMap: {}, // {personId: {lastAwareTimestamp: num, person: personObj}}
+//   resilience: 5, // num between 1 and 15
+// }
